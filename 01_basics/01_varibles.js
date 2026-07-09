7e78672db5165f6f7a8d1b88ffb6613e3c2a1019
Vivekananda-2303 <166122465+Vivekananda-2303@users.noreply.github.com>
2026-07-09 11:41:35 +0000

    variables

---
+++ 01_basics/01_varibles.js
@@ -0,0 +1,17 @@
+const accountId=12343
+let accountEmail="viveksp0011@gmail"
+var accountPassword="jaipur"
+accountCity="jaipur"
+//accountId=2// not allowed
+
+accountEmail="vsp0022@gmail"
+accountPassword="2323231"
+accountCity="bangalore"
+let accountState ;
+
+/*
+prefer not to use var ,becoz of issue in block scope and functional scope
+
+*/
+
+console.table([accountEmail,accountId,accountPassword,accountCity])
