c31266fbffc831adc4c867849000e5ff5b625d53
Vivekananda-2303 <166122465+Vivekananda-2303@users.noreply.github.com>
2026-07-09 11:39:49 +0000

    datatypes

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict";//treat all js code as neweer version
+
+//alert(3+3); we are using nodejs ,not browser 
+console.log(3+3)
+ console.log("vivek")
+ let name ="vivek"
+ let age=23
+ let isloggedIn=false
+
+ // number =>2 to power 53
+ //bigint
+ //String =>""
+//boolean=> false, true 
+//null=> standalone value 
+// undefined =>
+//symbol=> unique
+
+
+//object=
+console.log(typeof age)
+console.log(typeof "vivek");
+console.log(typeof null);
+console.log(typeof undefined);
+
+
+
