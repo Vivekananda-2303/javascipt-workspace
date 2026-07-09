817ec0cf4eb8244c0848871b044ba8656e4948ed
Vivekananda-2303 <166122465+Vivekananda-2303@users.noreply.github.com>
2026-07-09 16:30:50 +0000

    conversionOperation

---
+++ 01_basics/01_conversionOperation.js
@@ -0,0 +1,53 @@
+ /* let score ="vivek"
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber =Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// 33=> 33
+//"33abc"=> NaN(not a Number )
+//true=>1,false =0;
+let isloggedIn=""
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+*/
+//operation
+let value=3
+let negValue=-value
+console.log(negValue);
+console.log(2**3);
+let str1 = "hello"
+let str2="vivek"
+let str3= str1+str2
+console.log(str3);
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+console.log(+true);
+console.log(+"");
+let num1,num2,num3
+num1=num2=num3=2+2;
+let gamecounter=100;
+++gamecounter ;
+console.log(gamecounter);
+let x = 3;
+const y = x++;
+console.log(x);
+console.log(y);
+
+let a = 3;
+const b = ++a;
+console.log(a);
+console.log(b);
+//link to  study https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+
+
+
+
+
+
+
