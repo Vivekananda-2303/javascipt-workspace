c04ada86b958d729a8035245ef151531b3a1a54e
Vivekananda-2303 <166122465+Vivekananda-2303@users.noreply.github.com>
2026-08-21 21:30:22 +0000

    01Arrays

---
+++ 02_basics/Arrays.js
@@ -0,0 +1,30 @@
+const myArr=[0,1,2,3,4]
+console.log(myArr[1]);
+const myplayer=["pedri","yamal"]
+const myArr2= new Array(1,2,3,4,5);
+// console.log(myArr2);
+//Array methods
+myArr2.push(7)
+myArr2.push(8)
+myArr2.push(9)
+myArr2.pop()
+// console.log(myArr2);
+// console.log(MyArr2.includes(9));
+// console.log(MyArr2.indexOf(3));
+// const  newArr = myArr2.join()
+// console.log(myArr2);
+// console.log(typeof newArr)
+//splice,slice
+console.log("A=",myArr2)
+const myn1=myArr.slice(1,3)
+console.log(myn1)
+console.log("B=",myArr2);
+const myn2= myArr2.splice(1,3);
+console.log("c=",myArr2)
+console.log(myn2)
+
+
+
+
+
+
