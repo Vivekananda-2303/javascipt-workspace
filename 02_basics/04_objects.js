d4da2447690d1ef55081e338a7cdea572f08a76f
Vivekananda-2303 <166122465+Vivekananda-2303@users.noreply.github.com>
2026-08-21 22:17:47 +0000

    04_objects and json

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -31,3 +31,117 @@ console.log(Object.entries(tinderUser));
 
 console.log(tinderUser.hasOwnProperty('isLoggedIn'));// boolean output
 
+const course={
+    course:"js in hindi",
+    price :"999",
+    courseInstuctor: "hitesh"
+}
+///destructuring of Object;
+course.courseInstuctor
+const {courseInstuctor:Instructor} = course
+const{price:amount }=course
+//console.log(courseInstuctor);
+//console.log(price);
+console.log(Instructor);
+console.log(amount )
+//pratice
+// const user={
+//     name:"alien",age:30,role:"admin"
+// }
+//const name= user.name
+//const age =user.age
+//const role =user.admin
+// const{name,age,role}=user;console.log(name);console.log(age);console.log(role);
+
+// const user1={
+//     name :"alex",address:{
+//         city:"seattle",
+//         zipCode:"54848"
+//     }
+// }
+// const {address : {city,zipCode}}= user;
+// console.log(city)
+const user={name :"alex",age:30,role :"admin",active:true}
+const{name,...otherDetails}=user;
+console.log(name);
+console.log(otherDetails)
+
+//API
+//json
+// {
+//     "name":"vivek",
+//     "course" :"javaScript Workspapce"
+//     "price":"free"
+// }
+[
+    {},
+
+    {},
+    {}
+]
+{
+  "results": [
+    {
+      "gender": "female",
+      "name": {
+        "title": "Miss",
+        "first": "Jennie",
+        "last": "Nichols"
+      },
+      "location": {
+        "street": {
+          "number": 8929,
+          "name": "Valwood Pkwy",
+        },
+        "city": "Billings",
+        "state": "Michigan",
+        "country": "United States",
+        "postcode": "63104",
+        "coordinates": {
+          "latitude": "-69.8246",
+          "longitude": "134.8719"
+        },
+        "timezone": {
+          "offset": "+9:30",
+          "description": "Adelaide, Darwin"
+        }
+      },
+      "email": "jennie.nichols@example.com",
+      "login": {
+        "uuid": "7a0eed16-9430-4d68-901f-c0d4c1c3bf00",
+        "username": "yellowpeacock117",
+        "password": "addison",
+        "salt": "sld1yGtd",
+        "md5": "ab54ac4c0be9480ae8fa5e9e2a5196a3",
+        "sha1": "edcf2ce613cbdea349133c52dc2f3b83168dc51b",
+        "sha256": "48df5229235ada28389b91e60a935e4f9b73eb4bdb855ef9258a1751f10bdc5d"
+      },
+      "dob": {
+        "date": "1992-03-08T15:13:16.688Z",
+        "age": 30
+      },
+      "registered": {
+        "date": "2007-07-09T05:51:59.390Z",
+        "age": 14
+      },
+      "phone": "(272) 790-0888",
+      "cell": "(489) 330-2385",
+      "id": {
+        "name": "SSN",
+        "value": "405-88-3636"
+      },
+      "picture": {
+        "large": "https://randomuser.me/api/portraits/men/75.jpg",
+        "medium": "https://randomuser.me/api/portraits/med/men/75.jpg",
+        "thumbnail": "https://randomuser.me/api/portraits/thumb/men/75.jpg"
+      },
+      "nat": "US"
+    }
+  ],
+  "info": {
+    "seed": "56d27f4a53bd5441",
+    "results": 1,
+    "page": 1,
+    "version": "1.4"
+  }
+}
