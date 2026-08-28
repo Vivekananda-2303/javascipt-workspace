7fe42b3baa95215ad00e8617820c112de07d412b
Vivekananda-2303 <166122465+Vivekananda-2303@users.noreply.github.com>
2026-08-28 22:16:49 +0000

    forEach2

---
+++ 05_iterations/six.js
@@ -0,0 +1,24 @@
+// const coding=["html","css","javascript","react","nodejs"];
+// coding.forEach((item)=>{
+//     console.log(item)
+// })
+// console.log(values)
+
+
+const no=[1,2,3,4,5,6,7,8,9,10];
+// const newnums=no.filter((num)=>num>5);
+// const newnums=no.filter((nums)=>{
+//     return nums>5;
+// })
+// // if scope is open return is mandatory;
+// console.log(newnums)
+
+//using forEach
+
+// const newnums=[];
+// no.forEach((num)=>{
+//     if(num >4){
+//         newnums.push(num);
+//     }
+// })
+// console.log(newnums)
