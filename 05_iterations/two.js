6cf99115118ac1098706c0dc571541d0b3b4b9d8
Vivekananda-2303 <166122465+Vivekananda-2303@users.noreply.github.com>
2026-08-28 22:10:13 +0000

    while,do while

---
+++ 05_iterations/two.js
@@ -0,0 +1,19 @@
+//while
+let index=0
+
+while(index<10){
+    console.log(`Value of index is ${index}`);
+    index++;
+}
+let myArray=['flash','batman','superman','wonderwomen'];
+let arr=0;
+while(arr<myArray.length){
+    console.log(`Value is ${myArray[arr]}`);
+    arr=arr+1;
+}
+//do-while loop
+let score=1;
+do{
+    console.log(`Value of score is ${score}`);
+    score++;
+} while(score<=10);
